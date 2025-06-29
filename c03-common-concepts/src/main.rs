cfd49835bc8c4bd18a653360f767c441449dd9e8
Jere Lantz <lantz.jere@gmail.com>
2025-06-29 14:43:00 +0300

    some notes on loops

--- c03-common-concepts/src/main.rs
+++ c03-common-concepts/src/main.rs
@@ -1,3 +1,62 @@
+fn loops(){
+    // rustissa on 3 eri tapaa loopata "loop", "for", "while"
+    // loop toistaa koodia kunnes se keskeytetään
+    // Loopista voi myös returnata tietoa breakin avulla
+    
+    let mut counter = 0;
+
+    let result = loop{
+        counter += 1;
+
+        if counter == 5{
+            break 69;
+        }
+    };
+
+    println!("Loopin tulos {result}");
+
+    // Rustissa voidaan myös nimetä looppeja ja break/continue muita looppeja kun se lähin mitä
+    // suoritetaan
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    // While loopit toimii samalla lailla kun kaikissa muissa kielissä
+    //
+    // For looppia käytetään kun halutaan iteroida joku array tms ja tehdä joka elementille jotain
+    let arr = [2, 4, 6,9];
+
+    for n in arr{
+        println!("array {}",n);
+    }
+
+    // for looppia voidaan käyttää myös antamalla sille joku range
+    for n in 1..4{
+        println!("counting :{}", n)
+    }
+
+    // metodilla .rev() voidaan kääntää range toisin päin
+    for n in (1..4).rev(){
+        println!("counting reverse :{}", n)
+    }
+}
+
 fn add69(x: i32) -> i32 {
     // rust palauttaa automaattisesti funktion viimeisen rivin arvon.
     // Jos halutaan returnata aiemmin on mahdollista käyttää return avainsanaa
@@ -60,5 +119,7 @@ fn main() {
     expression(69);
 
     let number = 420;
-    println!("{} plus 69 = {}", number, add69(number))
+    println!("{} plus 69 = {}", number, add69(number));
+
+    loops();
 }
